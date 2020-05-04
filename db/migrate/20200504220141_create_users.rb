91c76e912516c443fe1486d7d6a72ec50b3a25cf
Michelle Kim <x1michelleskim@gmail.com>
2020-05-04 15:34:10 -0700

    Add helper methods to application controller for current_user and to login, logout users

--- db/migrate/20200504220141_create_users.rb
+++ db/migrate/20200504220141_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :password_digest, null: false
       t.string :session_token, null: false
       t.timestamps
-    endg
+    end
     add_index :users, :email, unique: true
     add_index :users, :session_token, unique: true
   end
