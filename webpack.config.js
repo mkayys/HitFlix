57c2acb0a1b3009ba4bb56d6479ebd2099223caa
Michelle Kim <x1michelleskim@gmail.com>
2020-05-04 14:12:22 -0700

    Initial setup for project in terms of npm packages and setting up the rails backend

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+
+module.exports = {
+    context: __dirname,
+    entry: './frontend/in_tents.jsx',
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css', '*']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                }
+            },
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /\.png$/, loader: "url-loader?limit=100000" },
+            { test: /\.jpg$/, loader: "file-loader" }
+        ]
+    },
+
+    devtool: 'inline-source-map'
+};
