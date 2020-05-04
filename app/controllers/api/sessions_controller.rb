d0027dafc4d1762f9fa3587cddc5aaa9dfa2af1a
Michelle Kim <x1michelleskim@gmail.com>
2020-05-04 16:49:54 -0700

    Connect the virtual dom through react on the frontend and test the response from controllers

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,25 @@
 class Api::SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by_cred(
+            params[:user][:email],
+            params[:user][:password]
+        )
+        if @user
+            login!(@user)
+            render 'api/users/show'
+        else
+            render json: ["Invalid email/password"], status: 420
+        end
+    end
+
+    def destroy
+        @user = current_user
+        if @user
+            logout!
+            render 'api/users/show'
+        else
+            render json: ["No one is signed in"], status: 418
+        end
+    end
 end
