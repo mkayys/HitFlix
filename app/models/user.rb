b27c449b26b449ee1fc6305a7adfe144222dad9e
Michelle Kim <x1michelleskim@gmail.com>
2020-05-04 15:24:53 -0700

    Write out user auth pattern in the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,34 @@
 class User < ApplicationRecord
+
+    validates :email, :password_digest, :session_token, presence: true
+    validates :email, :session_token, uniqueness: true
+    
+    attr_reader :password
+
+    before_validation :ensure_session_token
+
+    def self.find_by_creds(email, pw)
+        user = User.find_by(email: email)
+        return nil unless user
+        user.is_password?(pw) ? user : nil
+    end
+
+    def password=(pw)
+        @password = pw
+        self.password_digest = BCrypt::Password.create(pw)
+    end
+
+    def is_password?(pw)
+        BCrypt::Password.new(self.password_digest).is_password?(pw)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
 end
