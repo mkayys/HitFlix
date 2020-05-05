be70624612d1caed7ec30157e22de51b1a10cd64
Michelle Kim <x1michelleskim@gmail.com>
2020-05-04 22:21:35 -0700

    Finish the signIn container to dispatch the correct actions

--- frontend/components/session_form/login_container.js
+++ frontend/components/session_form/login_container.js
@@ -1,15 +1,17 @@
 import { connect } from "react-redux";
 import SessionForm from './session_form';
 
+import { signIn } from '../../actions/session_actions';
+
 const mSTP = (state) => {
     return {
-        
+        type: 'Sign In'
     }
 };
 
 const mDTP = (dispatch) => {
     return {
-
+        processForm: (user) => dispatch(signIn(user))
     }
 };
 
