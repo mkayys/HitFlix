4b4296e3bf889b626f88125dd4b0bcf4e4a857dc
Michelle Kim <x1michelleskim@gmail.com>
2020-05-04 17:26:22 -0700

    Write out the case statements for the reducers when receiving a current user

--- frontend/hit_flix.jsx
+++ frontend/hit_flix.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
+
 document.addEventListener("DOMContentLoaded", () => {
 
     const root = document.getElementById('root');
+    const store = configureStore();
+
+    // TESTING
+    window.store = store;
+    //
 
     ReactDOM.render(<h1>HitFlix</h1>, root)
 })
