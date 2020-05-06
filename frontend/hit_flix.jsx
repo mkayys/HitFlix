3a64087595ccb57bfbc96f2b75878c2d875d5500
Michelle Kim <x1michelleskim@gmail.com>
2020-05-05 19:34:54 -0700

    Current user persists on the frontend upon refresh

--- frontend/hit_flix.jsx
+++ frontend/hit_flix.jsx
@@ -9,7 +9,18 @@ import Root from './components/root';
 document.addEventListener("DOMContentLoaded", () => {
 
     const root = document.getElementById('root');
-    const store = configureStore();
+
+    let preloadedState;
+    
+    if (window.currentUser) {
+        preloadedState = {
+            session: {
+                currentUser: window.currentUser
+            }
+        };
+    };
+
+    const store = configureStore(preloadedState);
 
     // TESTING
     window.store = store;
