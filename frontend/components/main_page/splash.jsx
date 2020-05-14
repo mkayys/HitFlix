38af84edcd181ba907f5da15ac8b0b59618a1e5d
Michelle Kim <x1michelleskim@gmail.com>
2020-05-13 22:30:49 -0700

    Work on getting a video clip as the background for the splash page

--- frontend/components/main_page/splash.jsx
+++ frontend/components/main_page/splash.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 const Splash = () => (
     <div>
+        <video autoPlay muted loop id="myVideo">
+            <source src={window.movies} type="video/mp4" />
+        </video>
         <h1>Welcome to HitFlix</h1>
         <h3>Sign Up here</h3>
     </div>
