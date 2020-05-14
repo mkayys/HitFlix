9b5e0af8f3576242a273dcd9d124f0d083c8ce5b
Michelle Kim <x1michelleskim@gmail.com>
2020-05-13 23:29:57 -0700

    Test different videos

--- frontend/components/main_page/splash.jsx
+++ frontend/components/main_page/splash.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Splash = () => (
     <div>
         <video autoPlay muted loop id="myVideo">
-            <source src={window.movies} type="video/mp4" />
+            <source src={window.remote} type="video/mp4" />
         </video>
         <h1>Welcome to HitFlix</h1>
         <h3>Sign Up here</h3>
